3c959ffd735afa391a60c0b0edb89f8de82113e0
Glitch (onetecglitch) <none>
2020-07-06 05:05:45 +0000

    🍛👨‍❤️‍👨 Updated with Glitch

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ const app = express();
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
+const BuscaCep = require('busca-cep');
+const CepCoords = require("coordenadas-do-cep");
+
+let nome = "";
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 // our default array of dreams
 const dreams = [
@@ -17,7 +23,45 @@ const dreams = [
   "Climb a really tall mountain",
   "Wash the dishes"
 ];
+//=======================================================================================================================
 
+
+
+app.post("/pizzas", function(request, response ){
+  
+  var intentName = request.body.queryResult.intent.displayName;
+  
+  if (intentName == "cep.consultar") {
+    
+    const endereco1 = "Sp, carapicuíba avenida francisco pignatari";
+    const endereco2 = "Sp, jandira rua elton silva";
+
+    CepCoords.getDistEntreCeps("06310390", "06600025")
+    .then(distancia => {
+      if(distancia> 0 && distancia < 4) {
+        response.json({ fulfillmentText:  "Este endereço fica a " + distancia + "Km de distância.\nA taxa de entrega é R$ 3,00" });
+       //retorna o mesmo 'distancia' da versão em promise
+      }else if(distancia > 4 && distancia < 8 ){
+        response.json({ fulfillmentText:  "Este endereço fica a " + distancia + "Km de distância.\nA taxa de entrega é R$ 4,00" });
+      }else {response.json({ fulfillmentText:  "Afim de manter a rapidez de nossas entregas não entregamos em endereços que ficam a " + distancia + "Km de distância" });}
+      
+    })
+    .catch(err => {
+      response.json({ fulfillmentText: "CEP inválido.\nPor favor, digite seu CEP sem traço ou verifique se o número digitado está correto e tente de novo. " });
+       //retorna o mesmo parâmetro 'err' da versão em promise
+    })
+      
+    
+    
+    
+  }
+  
+    
+});
+
+
+
+//=================================================================================
 app.post("/OneTec", function(request, response) {
   var connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
@@ -29,8 +73,8 @@ app.post("/OneTec", function(request, response) {
   connection.connect();
 
   var intentName = request.body.queryResult.intent.displayName;
-  //### CRIAR CADASTRO ###//
-  if (intentName == "addCadastro") {
+  //==================== ### CRIAR CADASTRO ### ====================//
+  if (intentName == "cadastro") {
     console.log("incluir");
 
     var cadastroNome = request.body.queryResult.parameters["nome"];
@@ -48,44 +92,64 @@ app.post("/OneTec", function(request, response) {
     connection.query(query, function(error, results, fields) {
       if (error) throw error;
       connection.end();
-        response.json({ fulfillmentText: "Contato Adicionado com sucesso!" });
+      response.json({ fulfillmentText: "Contato Adicionado com sucesso!" });
     });
-    //### PESQUISAR CADASTRO ###//
-  }else if(intentName == "Identificar") {
+  } //==================== ### PESQUISAR CADASTRO ### ====================//
+  else if (intentName == "identificar.cliente") {
     console.log("Identificando cliente");
-    
+
+    let nome = "";
     let telContato = request.body.queryResult.parameters["telefone"];
-    let query = 'select * from Cadastro where telefone = "'+telContato+'"';
-    
-    connection.query( query, function( error, results, fields ){
-      if( results[0]){
-        var contato = 'Seu nome é...'+results[0].nome;
-        response.json({ "fulfillmentText": contato })
-      
-      } else{
-          /*let richResponses = [
-                {
-                    "quickReplies": {
-                        "title": "Você ainda não possui cadastro. Por favor, clique no botão abaixo",
-                        "quickReplies": [
-                            "Cadastrar"
-                        ]
-                    },
-                    "platform": "FACEBOOK"
-                }
-            ] 
-        response.json({ fulfillmentMessages: richResponses});*/
-        agent.setFollowupEvent("aaCadastro");
+    let query = 'select * from Cadastro where telefone = "' + telContato + '"';
+
+    connection.query(query, function(error, results, fields) {
+      if (results[0]) {
+        nome = results[0].nome;
+        let respostaIdentificar = [
+          {
+            quickReplies: {
+              title: "Seu nome é " + nome + "?",
+              quickReplies: ["Correto", "Não sou eu"]
+            },
+            platform: "FACEBOOK"
+          }
+        ];
+        response.json({ fulfillmentMessages: respostaIdentificar });
+      } else {
+        let respostaIdentificar = [
+          {
+            quickReplies: {
+              title:
+                "Você ainda não possui cadastro. Por favor, clique no botão abaixo...",
+              quickReplies: ["Cadastrar"]
+            },
+            platform: "FACEBOOK"
+          }
+        ];
+        response.json({ fulfillmentMessages: respostaIdentificar });
+        //agent.setFollowupEvent("aadCadastro");
       }
     });
     connection.end();
-             
+  } //==================== ### PEDIDO ### ====================//
+  else if (intentName == "fazer.pedido") {
+    console.log("Fazendo pedido");
+    
+    let respostaPedidos = [
+      {
+        quickReplies: {
+          title: "Seja muito bem vindo " + nome + "!\n" + "O que deseja pedir?",
+          quickReplies: ["Pizzas", "Bebidas"]
+        },
+        platform: "FACEBOOK"
+      }
+    ];
+    response.json({ fulfillmentMessages: respostaPedidos });
   }
   
 });
 
-// ####### CHAMAR CADASTRO #######
-//agent.setFollowupEvent("Pedidos");
+
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
@@ -105,4 +169,3 @@ app.get("/dreams", (request, response) => {
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
-
