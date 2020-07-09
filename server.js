8bbbd0d282121d894f660e06186c287984ad1d84
robsonmessias <robson.avafx@yahoo.com.br>
2020-07-09 04:07:09 -0300

    Revert "Update server.js"
    This reverts commit f0aa68eb260b058ad9b999b541eacbbfb66636f0.

--- server.js
+++ server.js
@@ -53,16 +53,20 @@ app.post("/pizzas", function(request, response ){
 
     CepCoords.getDistEntreCeps(endereco1, endereco2)
     .then(distancia => {
+      if(sem_cep == ''){
+        
         if(distancia < 4) {
           response.json({ fulfillmentText:  "Este endereço fica a menos de 4 Km de distância.\nA taxa de entrega é R$ 3,00" });
          //retorna o mesmo 'distancia' da versão em promise
         }else if(distancia > 4 && distancia < 8 ){
           response.json({ fulfillmentText:  "Este endereço fica a mais de 4 Km de distância.\nA taxa de entrega é R$ 4,00" });
-        }else {response.json({ fulfillmentText:  "Afim de manter a rapidez de nossas entregas não entregamos em endereços que ficam a mais de 8 Km de distância" });
-        }
+        }else {response.json({ fulfillmentText:  "Afim de manter a rapidez de nossas entregas não entregamos em endereços que ficam a mais de 8 Km de distância" });}
+        
+      }else {"Nossa taxa de entrega é de acordo com distância :\nAté  4 km.          R$3,00.\nDe 4 até  8km.  R$4,00.\nNÃO entregamos acima 8 km de distância para manter a rapidez de nossas entregas!"}
+      
     })
     .catch(err => {
-      response.json({ fulfillmentText: "Nossa taxa de entrega é de acordo com distância :\nAté  4 km.          R$3,00.\nDe 4 até  8km.  R$4,00.\nNÃO entregamos acima 8 km de distância para manter a rapidez de nossas entregas!"});
+      response.json({ fulfillmentText: "CEP inválido.\nPor favor, digite seu CEP sem traço ou verifique se o número digitado está correto e tente de novo. " });
        //retorna o mesmo parâmetro 'err' da versão em promise
     })
   }
