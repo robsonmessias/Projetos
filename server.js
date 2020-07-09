550279f18f5a3f34154aa0e4ec5e41466e43d231
robsonmessias <robson.avafx@yahoo.com.br>
2020-07-09 05:17:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -54,6 +54,9 @@ app.post("/pizzas", function(request, response ){
     CepCoords.getDistEntreCeps(endereco1, endereco2)
     .then(distancia => {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of f0aa68e... Update server.js
       if(sem_cep == ''){
         
         if(distancia < 4) {
@@ -64,6 +67,7 @@ app.post("/pizzas", function(request, response ){
         }else {response.json({ fulfillmentText:  "Afim de manter a rapidez de nossas entregas não entregamos em endereços que ficam a mais de 8 Km de distância" });}
         
       }else {"Nossa taxa de entrega é de acordo com distância :\nAté  4 km.          R$3,00.\nDe 4 até  8km.  R$4,00.\nNÃO entregamos acima 8 km de distância para manter a rapidez de nossas entregas!"}
+<<<<<<< HEAD
 =======
       if(distancia < 4) {
         response.json({ fulfillmentText:  "Este endereço fica a menos de 4 Km de distância.\nA taxa de entrega é R$ 3,00" });
@@ -76,6 +80,12 @@ app.post("/pizzas", function(request, response ){
     })
     .catch(err => {
       response.json({ fulfillmentText: "Nossa taxa de entrega é de acordo com distância :\nAté  4 km.          R$3,00.\nDe 4 até  8km.  R$4,00.\nNÃO entregamos acima 8 km de distância para manter a rapidez de nossas entregas!" });
+=======
+      
+    })
+    .catch(err => {
+      response.json({ fulfillmentText: "CEP inválido.\nPor favor, digite seu CEP sem traço ou verifique se o número digitado está correto e tente de novo. " });
+>>>>>>> parent of f0aa68e... Update server.js
        //retorna o mesmo parâmetro 'err' da versão em promise
     })
   }
